d6a38173a7eb8743c67f28ab326529019d9be7d4
arpitha-m-g <109780927+arpitha-m-g@users.noreply.github.com>
2023-01-03 21:21:41 +0530

    initial commit

---
+++ Jenkinsfile_file.groovy
@@ -0,0 +1,46 @@
+import java.time.*
+import java.time.format.DateTimeFormatter
+
+properties([parameters([
+    [$class: 'ChoiceParameter', 
+	    choiceType: 'PT_SINGLE_SELECT', 
+		filterLength: 1, 
+		filterable: false, 
+		name: 'environment', 
+		randomName: 'choice-parameter-126266809345300', 
+		script: [
+		    $class: 'GroovyScript', 
+			fallbackScript: [
+			    classpath: [], 
+				oldScript: '', 
+				sandbox: false, 
+				script: 
+				        'return ["error"]'
+			],
+			script: [
+			    classpath: [], 
+				oldScript: '', 
+				sandbox: false, 
+				script: 
+				        'return ["npe", "uat", "prod"]'
+				]
+			]
+		]
+	])
+])
+node(node_label) {
+  stage("Checkout Scm"){
+      checkout scm
+  }
+  stage("Display output"){
+      def json_output = '''
+          {
+           "author" : "${env.BUILD_USER_ID}",
+           "environment" : "param.environment",
+           "result": "{currentbuild.duration}"
+           }
+           '''
+      writeFile(file: 'output.json', text:json_output)  
+  }
+}
+            
