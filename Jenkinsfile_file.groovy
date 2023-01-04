04b7db87c20f205b7bc8d272f5910686ee8b2670
arpitha-m-g <109780927+arpitha-m-g@users.noreply.github.com>
2023-01-04 16:06:33 +0530

    Update Jenkinsfile_file.groovy

--- Jenkinsfile_file.groovy
+++ Jenkinsfile_file.groovy
@@ -35,7 +35,7 @@ node {
     stage("Display output"){
         def json_output = 
             ["author" : "${env.BUILD_USER_ID}",
-             "result":  "${currentbuild.duration}"
+             "result":  "${params.environment}"
             ]
              
 	writeFile(file: 'output.json', text:json_output) 
